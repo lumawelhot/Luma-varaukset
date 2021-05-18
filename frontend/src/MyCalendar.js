341997e17e9a510e423c6cdca0bfb9ca0e06b3ec
Pontus <pontus.hedlund@gmail.com>
2021-05-18 14:16:41 +0300

    First test commit
    Co-authored-by: Tapio Salonen <tsa-dom@users.noreply.github.com>
    Co-authored-by: mohkula <mohkula@users.noreply.github.com>
    Co-authored-by: jenkarper <jenkarper@users.noreply.github.com>
    Co-authored-by: ankaleh <ankaleh@users.noreply.github.com>

---
+++ frontend/src/MyCalendar.js
@@ -0,0 +1,20 @@
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
+
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
+
+const MyCalendar = props => (
+  <div>
+    <BigCalendar
+      localizer={localizer}
+      events={null}
+      startAccessor="start"
+      endAccessor="end"
+    />
+  </div>
+)
+
+export default MyCalendar
