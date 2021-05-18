b0a4362e3ad12a5d566264b6e7b4574b5c8c747f
Pontus <pontus.hedlund@gmail.com>
2021-05-18 14:18:07 +0300

    Front+Back

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyCalendar from './MyCalendar'
+
+function App() {
+  return (
+    <div className="App">
+      <MyCalendar/>
+    </div>
+  );
+}
+
+export default App;
