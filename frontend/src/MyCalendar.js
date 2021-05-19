58b5c7d99123d0e61b27ef3449027b19bf4f79e4
Matias Himanen <matias.himanen@helsinki.fi>
2021-05-19 15:01:16 +0300

    Walking skeleton completed

--- frontend/src/MyCalendar.js
+++ frontend/src/MyCalendar.js
@@ -1,21 +1,37 @@
 import React from 'react'
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 //import 'react-big-calendar/lib/css/react-big-calendar.css'
 import 'moment/locale/fi'
+import * as dates from 'react-big-calendar/lib/utils/dates'
 
 const localizer = momentLocalizer(moment)
 
+let allViews = Object.keys(Views).map(k => Views[k])
+
+const resourceMap = [
+  { resourceId: 1, resourceTitle: 'Summamutikka' },
+  { resourceId: 2, resourceTitle: 'Fotoni' },
+  { resourceId: 3, resourceTitle: 'Linkki' },
+  { resourceId: 4, resourceTitle: 'Geopiste' },
+  { resourceId: 5, resourceTitle: 'Gadolin' },
+]
 
 const MyCalendar = ({ events }) => (
   <div>
     <Calendar
       culture='fi'
       localizer={localizer}
+      views={allViews}
+      showMultiDayTimes
       events={events}
       startAccessor='start'
       endAccessor='end'
       style={{ height: 500 }}
+      max={dates.add(dates.endOf(new Date(2021, 7, 1), 'day'), -1, 'hours')}
+      resources={resourceMap}
+      resourceIdAccessor="resourceId"
+      resourceTitleAccessor="resourceTitle"
     />
   </div>
 )
--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const getEvents = async () => {
   const response = await axios.get('http://localhost:3001/events')
+  response.data.forEach(element => {
+    element.start = new Date(element.start)
+    element.end = new Date(element.end)
+  })
   return response.data
 }
 
