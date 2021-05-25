8897e68878463a15c5655348b279af583202a81e
Tapio Salonen <tsa.dom@pm.me>
2021-05-25 14:17:28 +0300

    add create user and login resolvers
    Co-authored-by: ankaleh <ankaleh@users.noreply.github.com>

--- backend/graphql/typeDefs.js
+++ backend/graphql/typeDefs.js
@@ -4,17 +4,25 @@ const typeDefs = gql `
   type User {
     id: ID!
     username: String!
+    isAdmin: Boolean!
+  }
+  type Token {
+    value: String!
   }
   type Query {
     getUsers: [User]!
   }
-`
-
-module.exports = typeDefs
-
-/*type Mutation {
+  type Mutation {
     createUser(
       username: String!
       password: String!
+      isAdmin: Boolean!
     ): User
-  }*/
+    login(
+      username: String!
+      password: String!
+    ): Token
+  }
+`
+
+module.exports = typeDefs
