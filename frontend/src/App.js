76ef1e15779339ab01495ae0fa58249c8586a10a
Pontus <pontus.hedlund@gmail.com>
2021-05-26 09:55:24 +0300

    ADD GraphQL Client in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,40 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 import MyCalendar from './MyCalendar'
 import apiService from './services/apiService'
+/*
+** GRAPHQL CLIENT
+** Remove comments when ready to implement
+
+import {
+  useQuery,
+  useApolloClient,
+  useMutation,
+  useSubscription,
+} from '@apollo/client'
+import { LOGIN } from './graphql/queries'
+*/
 
 const App = () => {
 
   const [events, setEvents] = useState([])
-
+  // GraphQL Client variables
+  // const client = useApolloClient()
+  // const [token, setToken] = useState(null)
+  // END GraphQL Client variables
 
   useEffect(() => {
     apiService.getEvents().then(data => setEvents(data))
   }, [])
+
+  /* USE TOKEN IF EXISTS
+  useEffect(() => {
+    const token = localStorage.getItem('libraryapp-token')
+    if (token) {
+      setToken(token)
+    }
+  }, [])
+
+  */
 
   return (
     <div className="App">
@@ -18,7 +43,5 @@ const App = () => {
     </div>
   )
 }
-
-// MUN FEATURE
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { setContext } from 'apollo-link-context'
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client'
+
 import './index.css'
 import App from './App'
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('libraryapp-token')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    },
+  }
+})
+
+const httpLink = new HttpLink({ uri: 'http://localhost:3001' })
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
