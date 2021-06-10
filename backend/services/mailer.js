b0a7feabc713d99da7df712442e50cf9bb2f52a6
Tapio Salonen <tsa.dom@pm.me>
2021-06-10 12:54:40 +0300

    Added file reader and mailer jest tests
    Co-authored-by: Pontus Hedlund <pontushed@users.noreply.github.com>

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const config = require('../utils/config')
 
-let transporter = nodemailer.createTransport({
+/* let transporter = nodemailer.createTransport({
   host: config.EMAILHOST,
   port: config.EMAILPORT,
   secure: false, // true for 465, false for other ports
@@ -9,16 +9,41 @@ let transporter = nodemailer.createTransport({
     user: config.EMAILUSER,
     pass: config.EMAILPASSWORD,
   },
-})
+}) */
 
-const sendMail = (mailDetails) => {
-  transporter.sendMail(mailDetails, (error) => {
-    if (error) {
-      //console.log(error);
-    } else {
-      //console.log('Email sent: ' + info.response)
-    }
+/* const sendMail = async (mailDetails) => {
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailDetails, (err, info) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(info)
+      }
+    })
+  })
+} */
+
+function sendMail(mailDetails) {
+  const senderSettings = {
+    host: config.EMAILHOST,
+    port: config.EMAILPORT,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: config.EMAILUSER,
+      pass: config.EMAILPASSWORD,
+    },
+  }
+  const transporter = nodemailer.createTransport(senderSettings)
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailDetails, (err, info) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(info)
+      }
+    })
   })
 }
 
-exports.sendMail = sendMail
+module.exports = { sendMail }
