8c4094547186e62b36ef84a25df2045869da4912
Tapio Salonen <tsa.dom@pm.me>
2021-06-10 14:07:45 +0300

    Remove comment

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -1,29 +1,7 @@
 const nodemailer = require('nodemailer')
 const config = require('../utils/config')
 
-/* let transporter = nodemailer.createTransport({
-  host: config.EMAILHOST,
-  port: config.EMAILPORT,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: config.EMAILUSER,
-    pass: config.EMAILPASSWORD,
-  },
-}) */
-
-/* const sendMail = async (mailDetails) => {
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(mailDetails, (err, info) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(info)
-      }
-    })
-  })
-} */
-
-function sendMail(mailDetails) {
+const sendMail = (mailDetails) => {
   const senderSettings = {
     host: config.EMAILHOST,
     port: config.EMAILPORT,
