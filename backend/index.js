51c90c28fa817647a17f2a65988aa2e853f7ed93
Pontus <pontus.hedlund@gmail.com>
2021-07-09 00:44:37 +0300

    Add config for node-cron

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,18 @@ const app = require('./app')
 const config = require('./utils/config')
 const PORT = config.PORT || 3001
 
+const cron = require('node-cron')
+
+cron.schedule('0 7 * * *', () => {
+  console.log('Running a task every day at 07:00')
+  // Lähetä muistutukset
+})
+
+cron.schedule('0 18 * * *', () => {
+  console.log('Running a task every day at 18:00')
+  // Lähetä kiitokset päivän vierailuista
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
