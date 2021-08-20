d580dd8403f7a7e568af9f5bd2b7566321cce72f
Pontus <pontus.hedlund@gmail.com>
2021-08-20 15:59:43 +0300

    Change mail settings for production

--- backend/services/mailer.js
+++ backend/services/mailer.js
@@ -2,15 +2,22 @@ const nodemailer = require('nodemailer')
 const config = require('../utils/config')
 
 const sendMail = (mailDetails) => {
-  const senderSettings = {
-    host: config.EMAILHOST,
-    port: config.EMAILPORT,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: config.EMAILUSER,
-      pass: config.EMAILPASSWORD,
-    },
-  }
+  const senderSettings = process.env.NODE_ENV === 'production' ?
+    {
+      host: config.EMAILHOST,
+      port: config.EMAILPORT,
+      secure: false, // true for 465, false for other ports
+    }
+    :
+    {
+      host: config.EMAILHOST,
+      port: config.EMAILPORT,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: config.EMAILUSER,
+        pass: config.EMAILPASSWORD,
+      },
+    }
   const transporter = nodemailer.createTransport(senderSettings)
 
   return new Promise((resolve, reject) => {
