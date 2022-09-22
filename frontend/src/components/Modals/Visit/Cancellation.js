bcb0731482b9f1258fd7cf44db91f9a4996dc07c
Tapio Salonen <tsa.dom@pm.me>
2022-09-22 21:03:17 +0300

    Added missing file changes

--- frontend/src/components/Modals/Visit/Cancellation.js
+++ frontend/src/components/Modals/Visit/Cancellation.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useMisc } from '../../../hooks/cache'
 import PropTypes from 'prop-types'
-import Title, { required } from '../../Embeds/Title'
+import Title, { Error, required } from '../../Embeds/Title'
 import { Input } from '../../Embeds/Input'
 import { Checkbox, CheckboxGroup, Radio, RadioGroup } from '../../Embeds/Button'
 import { useForm } from 'react-hook-form'
 import { cancelInitialValues } from '../../../helpers/initialvalues'
 import { parseCancellation } from '../../../helpers/parse'
+import { CancelFormValidation } from '../../../helpers/validate'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { useTranslation } from 'react-i18next'
 
 const Cancellation = ({ formId, show, onSubmit }) => {
   const { cancelForm: form } = useMisc()
+  const { t } = useTranslation()
 
-  const { control, register, handleSubmit } = useForm({
-    //resolver: !visit ? yupResolver(VisitValidation(form)) : undefined,
+  const { control, register, handleSubmit, formState: { errors }, setValue } = useForm({
+    resolver: yupResolver(CancelFormValidation(form)),
     defaultValues: form ? cancelInitialValues(form) : undefined,
     mode: 'onTouched'
   })
+
+  useEffect(() => form?.fields && form?.fields?.forEach((field, j) => setValue(`custom-${j}`, field.type === 'checkbox' ? [] : '')), [])
 
   return (
     <form id={formId} onSubmit={handleSubmit(v => onSubmit(parseCancellation({ ...v, form })))} className={show ? 'visible' : 'hidden'}>
@@ -32,7 +38,7 @@ const Cancellation = ({ formId, show, onSubmit }) => {
           {type === 'checkbox' && <CheckboxGroup name={`custom-${j}`} control={control} render={<>
             {field.options.map((o, i) => <Checkbox key={i} value={o.text.toString()} >{o.text}</Checkbox>)}
           </>} />}
-          {/* errors[`custom-${j}`] && <Error>{t('fill-field')}</Error> */}
+          {errors[`custom-${j}`] && <Error>{t('fill-field')}</Error>}
         </div>
       })}
 
