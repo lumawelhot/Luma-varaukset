b3cfb1484685a1ae0549a585241fc027b16f7092
Tapio Salonen <tsa.dom@pm.me>
2023-02-09 22:56:59 +0200

    Create setup-mongo-docker.js

---
+++ backend/setup-mongo-docker.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { initializeDB } = require('./services/dbsetup')
+mongoose.set('strictQuery', true)
+
+mongoose.connect('mongodb://localhost:27017')
+  .then(async () => {
+    console.log('Connected to MongoDB')
+    await initializeDB()
+    console.log('MongoDB initialized')
+    mongoose.connection.close()
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB: ', error.message)
+    mongoose.connection.close()
+  })
