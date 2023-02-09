76f4c79ce785e58a0d440439382a00910f3cefd1
Tapio Salonen <tsa.dom@pm.me>
2023-02-09 22:55:31 +0200

    Update run-mongo-docker.sh

--- run-mongo-docker.sh
+++ run-mongo-docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Starting MongoDB in background..."
 docker network create mongo
-docker run --name mongo --network mongo -p 27017:27017 -d mongo:latest
+docker run --name mongo --network mongo -p 27017:27017 -d mongo:focal
 docker run --name mongo-express --network mongo -p 8081:8081 -d mongo-express
 echo "MongoDB should now be up and listening on mongodb://localhost:27017/"
 echo "The admin UI is available at http://localhost:8081/"
+node backend/setup-mongo-docker.js
