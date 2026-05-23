9ea68ffc5ba931db8c336e68457cb328461826d1
Erik Peteri <peteri.erik@gmail.com>
2026-05-23 14:55:08 +0300

    hide radio buttons without labels

--- frontend/src/components/Modals/Visit/Form.js
+++ frontend/src/components/Modals/Visit/Form.js
@@ -82,6 +82,7 @@ const Form = ({ formId, show, onSubmit, event, visit }) => {
           {event?.remotePlatforms
             .map(j => PLATFORMS.map((_, i) => i + 1).includes(j)
               ? [PLATFORMS[j - 1], j] : [event.otherRemotePlatformOption, j])
+            .filter(([label]) => !!label && label.trim() !== '')
             .map(r => <Radio key={r[1]} value={`${r[0]}`}>{r[0]}</Radio>)
           }
           <Radio value={`${event?.remotePlatforms.length + 2}`}>{t('other-what')}</Radio>
